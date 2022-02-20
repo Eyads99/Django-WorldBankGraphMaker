6bbc72735de388c28588f84741f210c9b88865d2
eyads <eyadshaban99@gmail.com>
2022-02-20 23:46:46 +0200

    Added test to tests.py

--- WB/tests.py
+++ WB/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+
+
+class TestViews(TestCase):
+
+    def test_index(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
