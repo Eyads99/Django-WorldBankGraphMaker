a7e4709ba686b66fb7a1895c23c8aef157a94234
eyads <eyadshaban99@gmail.com>
2022-02-21 20:46:05 +0200

    Added extra test for landing page

--- WB/tests.py
+++ WB/tests.py
@@ -7,6 +7,10 @@ from django.urls import reverse
 
 class TestViews(TestCase):
 
-    def test_index(self):
+    def test_index(self):  # test index page (world bank graph making page)
         response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home(self):  # test home page
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
