ba45469b16d4b0ad89e6e8dcc1ed33809bf05ccb
eyads <eyadshaban99@gmail.com>
2022-02-24 14:48:38 +0200

    Fixed error in test and added 1 test

--- WB/tests.py
+++ WB/tests.py
@@ -8,13 +8,19 @@ from django.urls import reverse
 class TestViews(TestCase):
 
     def test_index(self):  # test index page (world bank graph making page)
-        response = self.client.get(reverse('index'))
+        response = self.client.get(reverse('WB:index'))
         self.assertEqual(response.status_code, 200)
 
-    def test_graph(self):  # test graph page with example GET request that returns no data
-        response = self.client.get(reverse('graph'),
-                                   data={'states': 'AFG', 'metrics': 'SP.POP.TOTL', 'year1': '2019', 'year2': '2021',
-                                         'title': '', 'x_label': '', 'y_label': '', 'auto_scale': '1'})
+    def test_graph_no_data(self):  # test graph page with example GET request that returns no data
+        response = self.client.get(reverse('WB:graph'),
+                                   data={'states': 'ABW', 'metrics': 'AG.AGR.TRAC.NO', 'year1': '2019', 'year2': '2021',
+                                         'title': '', 'xlabel': '', 'ylabel': '', 'auto_scale': '1'})
+        self.assertEqual(response.status_code, 200)
+
+    def test_graph_data(self):  # test graph page with example GET request that returns data
+        response = self.client.get(reverse('WB:graph'),
+                                   data={'states': 'EGY', 'metrics': 'NY.GDP.MKTP.CD', 'year1': '1960', 'year2': '2021',
+                                         'title': '', 'xlabel': '', 'ylabel': '', 'auto_scale': '1'})
         self.assertEqual(response.status_code, 200)
 
     # def test_home(self):  # test home page
