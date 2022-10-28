b78b322f230b59433a638420fabfeb1487bcc400
Eyads99 <eyadshaban99@gmail.com>
2022-10-28 08:45:35 +0200

    Refactored code

--- WB/urls.py
+++ WB/urls.py
@@ -8,7 +8,7 @@ app_name = 'WB'
 urlpatterns = [
     # /WB/+url path , view to use, name
     path(r"", views.index, name='index'),
-    path(r"index1", views.index1, name='index1'),
+    path(r"index1", views.render_info, name='index1'),
     path(r"getname", views.get_name, name='name'),
     path(r"graph", views.graph, name='graph'),
     # path(r"download_CSV", views.download_CSV, name="download_CSV"),
