c1ceb2e7d5e219df162dd020d19e8a60c0390a1b
Eyads99 <eyadshaban99@gmail.com>
2022-11-03 20:27:00 +0200

    bokeh display added

--- WB/bokehGraph.py
+++ WB/bokehGraph.py
@@ -1,11 +1,29 @@
 from bokeh.io import save
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, show
+from bokeh.embed import components
+from bokeh.resources import INLINE
 import pandas as pd
 
 
 def make_bokeh_graph(DF, country_codes, metric_list, start_year=1960, end_year=2020, title='', xlabel='Year', ylabel='',
                      height=7, width=35, kind='line', black_and_white=False, ylim=None, xlim=None):
+    pass
+
+    source = ColumnDataSource({str(c): v.values for c, v in DF.transpose().items()})
+
+    p = figure(title=title, x_axis_label=xlabel, y_axis_label=ylabel, x_axis_type="datetime")
+
+    p.line(line_width=2, source=source)
+
+    script, div = components(p)
+
+    return script, div, INLINE.render()
+
+    # show(p)
+
+    # return p.line(x=DF.index.values, y=DF, source=ColumnDataSource(DF))
+
     DF = ColumnDataSource(DF)
 
     graph = figure(source=DF, title=title, x_axis_label=xlabel, y_axis_label=ylabel)
