f9fcb625dc9aad5fda6a417526b14e750addf8b4
Eyads99 <eyadshaban99@gmail.com>
2023-01-10 19:24:49 +0200

    Improved bokeh highlight

--- WB/bokehGraph.py
+++ WB/bokehGraph.py
@@ -8,7 +8,7 @@ import wbgapi as wb
 
 
 def make_bokeh_graph(DF, country_codes, metric_list, start_year=1960, end_year=2020, title='', xlabel='Year', ylabel='',
-                     height=7, width=35, kind='line', black_and_white=False):
+                     height=7, width=35, black_and_white=False):
     if DF is None or len(DF) == 0 or DF.empty is True:
         return None
 
@@ -23,41 +23,46 @@ def make_bokeh_graph(DF, country_codes, metric_list, start_year=1960, end_year=2
 
     source = ColumnDataSource({str(c): v.values for c, v in DF.transpose().items()})
 
-    p = figure(title=title, x_axis_label=xlabel, y_axis_label=ylabel)
+    tooltip = make_tool_tip(metric_list=metric_list, country_list=country_codes, source=source)
+
+    p = figure(title=title, x_axis_label=xlabel, y_axis_label=ylabel, tooltips=tooltip)
 
     if source.column_names[0] == "Year":
+
+        for key in source.data.keys():
+            source.data[key] = list(source.data[key])
+
         for idx, data in enumerate(source.column_names[1:]):  # first row is year
             p.line(x=source.column_names[0], y=data, line_width=2, source=source,
-                   legend_label=make_legend_title(data, len(country_codes) > 1, len(metric_list) > 1),
+                   legend_label=make_readable_title(data, len(country_codes) > 1, len(metric_list) > 1),
                    color=Category10[10][idx])  # to make line graph
             p.circle(x=source.column_names[0], y=data, source=source,
-                     legend_label=make_legend_title(data, len(country_codes) > 1, len(metric_list) > 1),
+                     legend_label=make_readable_title(data, len(country_codes) > 1, len(metric_list) > 1),
                      color=Category10[10][idx])  # to put dots on the line graph
 
-        p.line(x=source.column_names[0], y=source.column_names[1], line_width=2,
-               source=source)  # to display line graph
-        p.circle(x=source.column_names[0], y=source.column_names[1], source=source)  # to put dots on the line graph
+        # p.line(x=source.column_names[0], y=source.column_names[1], line_width=2,
+        #        source=source)  # to display line graph
+        # p.circle(x=source.column_names[0], y=source.column_names[1], source=source)  # to put dots on the line graph
     else:  # multiple countries and metrics
         for idx, data in enumerate(source.column_names[:-1]):
             p.line(x=source.column_names[-1], y=data, line_width=2, source=source,
-                   legend_label=make_legend_title(data, True, True), color=Category10[10][idx])  # to make line graph
+                   legend_label=make_readable_title(data, True, True), color=Category10[10][idx])  # to make line graph
             p.circle(x=source.column_names[-1], y=data, source=source,
-                     legend_label=make_legend_title(data, True, True),
+                     legend_label=make_readable_title(data, True, True),
                      color=Category10[10][idx])  # to put dots on the line graph
 
     p.yaxis.formatter = BasicTickFormatter(use_scientific=False, precision=2)  # prevent use of scientific notation
 
-
-    script, div = components(p)
+    script, div = components(p)  # return JS script and HTML code for graph
 
     return script, div, INLINE.render()
 
 
-def make_legend_title(data, multiple_countries=False, multiple_metrics=False) -> str:  # TODO read country code & metric
+def make_readable_title(data, multiple_countries=False,
+                        multiple_metrics=False) -> str:
 
     if multiple_countries and multiple_metrics:  # if multiple countries and indicators
-        data = data.replace("(", "").replace(')', '').replace("'", "").replace(" ",
-                                                                               "")  # remove apostrophes and brackets
+        data = data.replace("(", "").replace(')', '').replace("'", "").replace(" ", "")  # remove apostrophe and bracket
         title = data.split(',')  # split title elements by comma
         return wb.economy.metadata.get(title[0]).metadata['ShortName'] + ": " \
             + wb.series.metadata.get(title[1]).metadata.get('IndicatorName')
@@ -66,3 +71,28 @@ def make_legend_title(data, multiple_countries=False, multiple_metrics=False) ->
         return wb.series.metadata.get(data).metadata.get('IndicatorName')  # return metric name
     else:
         return wb.economy.metadata.get(data).metadata['ShortName']  # return country name
+
+
+def make_tool_tip(metric_list, country_list, source):
+    tooltip = [
+        # ("Value", "@AFG"),
+        # ("Year", "@Year")
+    ]
+
+    len_metric_list = len(metric_list)
+    len_country_list = len(country_list)
+
+    if (len_country_list + len_metric_list) == 2:  # single country single metric
+        pass  # tooltip.append((make_readable_title(country_list[0], False, False), "@"+country_list[0]))
+    elif len_metric_list > 1 and len_country_list > 1:  # multiple countries and metrics
+        tooltip = [(make_readable_title(col_name, True, True), "@{" + col_name + "}")
+                   for col_name in source.column_names[:-1]]
+        tooltip.append(("Year", " @{('Year', '')} "))  # multi country and metric datasource handles years differently
+        return tooltip
+    elif len_metric_list > 1:  # multiple metrics single country
+        tooltip = [(make_readable_title(metric, False, True), "@{" + metric + "}") for metric in metric_list]
+    else:  # single metric and multiple countries
+        tooltip = [(make_readable_title(country, True, False), "@{" + country + "}") for country in country_list]
+
+    tooltip.append(("Year", "@Year"))
+    return tooltip
